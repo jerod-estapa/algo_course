49231f05770b68ec443ca88ed0990cdd4901be48
Jerod Estapa <jerodestapa@gmail.com>
2015-06-22 12:56:20 -0500

    Fixed whitespace in logic_gate.py

--- logic_gate.py
+++ logic_gate.py
@@ -5,12 +5,12 @@
 class LogicGate:
 
     def __init__(self, n):
-						self.label = n
-						self.output = None
+        self.label = n
+        self.output = None
 
-		def get_label(self):
-						return self.label
+    def get_label(self):
+        return self.label
 
-		def get_output(self):
-						self.output = self.perform_gate_logic()
-						return self.output
+    def get_output(self):
+        self.output = self.perform_gate_logic()
+        return self.output
