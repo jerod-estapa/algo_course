5dc1426edcbf1f110ef72b7ddc642846b5c10755
Jerod Estapa <jerodestapa@gmail.com>
2015-06-24 14:00:17 -0500

    Added AndGate class

--- logic_gate.py
+++ logic_gate.py
@@ -44,3 +44,17 @@ class UnaryGate(self, n):
     def get_pin(self):
         return int(input("Enter Pin input for gate " + self.get_label()+"-->"))
 
+
+class AndGate(BinaryGate):
+
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def perform_gate_logic(self):
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
