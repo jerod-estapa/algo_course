4d9b65cb014adde028cc39e83d9b31aacd41b3d0
jerodestapa <jerodestapa@gmail.com>
2015-06-28 10:55:26 -0500

    Added docstrings

--- intro/fraction_class.py
+++ intro/fraction_class.py
@@ -12,42 +12,50 @@ def gcd(m, n):
 
 
 class Fraction:
+
+
     def __init__(self, top, bottom):
         self.num = top
         self.den = bottom
 
+# string method
     def __str__(self):
         return str(self.num)+"/"+str(self.den)
 
+# print method
     def show(self):
         print(self.num, "/", self.den)
 
+# add method (adds fractions)
     def __add__(self,otherfraction):
-        newnum = self.num*otherfraction.den + \
-                    self.den*otherfraction.num
+        newnum = self.num * otherfraction.den + self.den *otherfraction.num
         newden = self.den * otherfraction.den
         common = gcd(newnum,newden)
         return Fraction(newnum//common,newden//common)
 
+# sets fractions as equal
     def __eq__(self, other):
         firstnum = self.num * other.den
         secondnum = other.num * self.den
         return firstnum == secondnum
 
-<<<<<<< HEAD
+# multiply method
     def __mul__(self, otherfraction):
-        newnum =
-=======
-     def __mul__(self, otherfraction):
-         mulnum = self.num * otherfraction.num
-         mulden = self.den * otherfraction.den
-         return Fraction(mulnum, mulden)
+        mulnum = self.num * otherfraction.num
+        mulden = self.den * otherfraction.den
+        return Fraction(mulnum, mulden)
 
-     def __div__(self, otherfraction):
-         divnum = self.num * otherfraction.den
-         divden = self.den * otherfraction.num
-         return Fraction(divnum, divden)
+# division method
+    def __div__(self, otherfraction):
+        divnum = self.num * otherfraction.den
+        divden = self.den * otherfraction.num
+        return Fraction(divnum, divden)
 
-     def __sub__(self, otherfraction):
-         subnum =
->>>>>>> 058f3ff7c061e6876e42133de4fc124878cdc748
+# subtraction method
+    def __sub__(self, otherfraction):
+        newnum = self.num * otherfraction.den - self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+
+        common = gcd(newnum, newden)
+
+        return Fraction(self.newnum // common, self.newden // common)
