aa14409d4167faa1ec0c2b4323d5aefd167c8553
Jerod Estapa <jerodestapa@gmail.com>
2015-06-29 17:16:52 -0500

    Added print lines

--- analysis/prog_exers.py
+++ analysis/prog_exers.py
@@ -9,25 +9,29 @@ import timeit
 
 t = timeit.Timer("l.index(5000)", "from __main__ import l")
 
-print("list.index()")
+print("l.index()")
 
 for i in range(100000, 10000001, 1000000):
     l = list(range(i))
     lt = t.timeit(number=1000)
     print("%15.5f" % lt)
 
-print("-----------------------------------------------------")
+print("-----------------------------------")
 
 # 2. Devise an experiment to verify that get_item and set_item are O(1) for dictionaries
 
 
+print("-----------------------------------")
 
 # 3. Devise an experiment that compares the performance of the del operator on lists and dictionaries.
 
 
+print("-----------------------------------")
 
 # 4. Given a list of numbers in random order, write an algorithm that
 #    works in O(nlog(n)) to find the kth smallest number in the list.
 
+
+print("-----------------------------------")
 
 # 5. Can you improve the previous problem to be linear?
