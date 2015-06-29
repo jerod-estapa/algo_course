ad1b7ab275f972f3fcbb8c1ddb3f3c63d44d3894
Jerod Estapa <jerodestapa@gmail.com>
2015-06-29 17:07:07 -0500

    Completed exercise 1

--- analysis/prog_exers.py
+++ analysis/prog_exers.py
@@ -7,7 +7,16 @@
 
 import timeit
 
-l = list(range(1000000))
 t = timeit.Timer("l.index(5000)", "from __main__ import l")
 
-print(t.timeit(number=1000)/1000)
+print("list.index()")
+
+for i in range(100000, 10000001, 1000000):
+    l = list(range(i))
+    lt = t.timeit(number=1000)
+    print("%15.5f" % lt)
+
+print("-----------------------------------------------------")
+
+# 2. Devise an experiment to verify that get_item and set_item are O(1) for dictionaries
+
