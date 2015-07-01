90b6d64983c2eb45f6f8d09dda7d22e3bab9d2d3
jerodestapa <jerodestapa@gmail.com>
2015-06-30 22:53:07 -0500

    Finished get test

--- analysis/prog_exers.py
+++ analysis/prog_exers.py
@@ -27,7 +27,7 @@ print("Exercise 2")
 print("----------")
 
 for i in range(10000, 1000001, 20000):
-    t = timeit.Timer("x.get(%d)" % i, "from __main__ import random, x")
+    t = timeit.Timer("x.get(random.randrange(%d))" % i, "from __main__ import random, x")
 
     x = {j: None for j in range(i)}
     get_time = t.timeit(number=1000)
