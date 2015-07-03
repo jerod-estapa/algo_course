18bea14db9ac266e04221fcc3692146427d69e61
jerodestapa <jerodestapa@gmail.com>
2015-07-03 16:42:54 -0500

    Exercise 5 algorithm for kth smallest number

--- analysis/prog_exers.py
+++ analysis/prog_exers.py
@@ -26,6 +26,7 @@ print("-----------------------------------")
 
 print("Exercise 2")
 print("----------")
+print("dictionary get method speed")
 
 for i in range(10000, 1000001, 20000):
     t = timeit.Timer("x.get(random.randrange(%d))" % i, "from __main__ import random, x")
@@ -39,16 +40,42 @@ print("-----------------------------------")
 
 # 3. Devise an experiment that compares the performance of the del operator on lists and dictionaries.
 
+print("Exercise 3")
 
 print("-----------------------------------")
 
 # 4. Given a list of numbers in random order, write an algorithm that
 #    works in O(nlog(n)) to find the kth smallest number in the list.
 
-def select(x, k):
-
-
+print("Exercise 4")
 
 print("-----------------------------------")
 
 # 5. Can you improve the previous problem to be linear?
+
+print("Exercise 5")
+
+def select(items, k):
+    items = list(items)
+    if not 0 <= k < len(items):
+        raise ValueError("Not enough elements for the given rank.")
+    while True:
+        pivot = random.choice(items)
+        pcount = 0
+        under, over = [], []
+        uappend, oappend = under.append, over.append
+        for elem in items:
+            if elem < pivot:
+                uappend(elem)
+            elif elem > pivot:
+                oappend(elem)
+            else:
+                pcount += 1
+        if k < len(under):
+            items = under
+        elif k < len(under) + pcount:
+            return pivot
+        else:
+            items = over
+            n -= len(under) + pcount
+
