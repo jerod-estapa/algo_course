f30e993ac7e717f93a63139f6ff7013ff2d12c9d
Jerod Estapa <jerodestapa@gmail.com>
2015-08-05 17:40:15 -0500

    Added unordered-list.py

---
+++ basic/unordered-list.py
@@ -0,0 +1,70 @@
+# Unordered List
+
+
+class Node:
+
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, newdata):
+        self.data = newdata
+
+    def set_next(self, newnext):
+        self.next = newnext
+
+
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(temp)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = self.get_next()
+
+        return count
+
+    def search(self):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
